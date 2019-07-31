ed350a3168d873b1bf5c1aee5d0d1d1a996cbaba
RuixeWolf <Ruixe1569898391@outlook.com>
2019-07-31 18:31:37 +0800

    修改script标签加载顺序为defer

--- Script/JavaScript/main.js
+++ Script/JavaScript/main.js
@@ -1,5 +1,19 @@
 // JavaScript Document
 
  var backgroundVideo = document.getElementById("backgroundVideo");
+                
+    function play(){
+        backgroundVideo.play();
+    }
+                
+    function pause(){
+        backgroundVideo.pause();
+    }
 
- 
+    function volumeUp(){
+        backgroundVideo.volume += 0.2;
+    }
+
+    function volumeDown(){
+        backgroundVideo.volume -= 0.2;
+    }
