2462c04fc24749d0d4518128f61aa1399f00cca1
RuixeWolf <Ruixe1569898391@outlook.com>
2020-01-05 14:45:10 +0800

    页面section1高度优化，登录注册页面高度优化，搜索栏聚焦后外边框变为橙色

--- Script/JavaScript/main.js
+++ Script/JavaScript/main.js
@@ -1,5 +1,7 @@
 // JavaScript Document
 
+
+// 相关创作页面播放
 var backgroundVideo = document.getElementById("backgroundVideo");
                 
 function play(){
@@ -17,3 +19,14 @@ function volumeUp(){
 function volumeDown(){
     backgroundVideo.volume -= 0.2;
 }
+
+
+// 导航栏搜索input聚焦后边框颜色变为#ff550a
+$("#searchInp").on("focus", function() {
+    $(this).parent().parent().css("border-color", "#ff550a");
+});
+
+// 导航栏搜索input失焦后边框颜色变为#2c2c2c
+$("#searchInp").on("blur", function() {
+    $(this).parent().parent().css("border-color", "#2c2c2c");
+});
